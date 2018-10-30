2641306c62ce68469d5e82d57a96c4f789de61a8
chipolatas <benjichiverton@gmail.com>
2018-10-30 00:46:23 +0000

    added suitable message.

--- Twitter/TweetListener.Engine/Persisters/TweetPersister.cs
+++ Twitter/TweetListener.Engine/Persisters/TweetPersister.cs
@@ -29,22 +29,28 @@ namespace TweetListener.Engine.Persisters
 
         private void Persist(string topic, TweetData tweetData)
         {
-            using (var dbConnection = new SqlConnection(_connectionString))
+            try
             {
-                var spParameters = new DynamicParameters();
-                spParameters.Add("@TweetId", tweetData.TweetId);
-                spParameters.Add("@Topic", topic);
-                spParameters.Add("@Content", tweetData.OriginalContent);
-                spParameters.Add("@TweetedTime", tweetData.TweetedTime);
-                if (tweetData.ReTweet)
+                using (var dbConnection = new SqlConnection(_connectionString))
                 {
-                    spParameters.Add("@OriginalTweetId", tweetData.OriginalTweetId);
-                    dbConnection.Execute("[dbo].[PersistReTweet]", spParameters, commandTimeout: 30, commandType: CommandType.StoredProcedure);
+                    var spParameters = new DynamicParameters();
+                    spParameters.Add("@TweetId", tweetData.TweetId);
+                    spParameters.Add("@Topic", topic);
+                    spParameters.Add("@Content", tweetData.OriginalContent);
+                    spParameters.Add("@TweetedTime", tweetData.TweetedTime);
+                    if (tweetData.ReTweet)
+                    {
+                        spParameters.Add("@OriginalTweetId", tweetData.OriginalTweetId);
+                        dbConnection.Execute("[dbo].[PersistReTweet]", spParameters, commandTimeout: 30, commandType: CommandType.StoredProcedure);
+                    }
+                    else
+                    {
+                        dbConnection.Execute("[dbo].[PersistTweet]", spParameters, commandType: CommandType.StoredProcedure);
+                    }
                 }
-                else
-                {
-                    dbConnection.Execute("[dbo].[PersistTweet]", spParameters, commandType: CommandType.StoredProcedure);
-                }
+            }catch (SqlException e) when (e.Number == 2627)
+            {
+                _log.Warn($"Tweet with Id: {tweetData.OriginalTweetId} already exists in dbo.TweetData.");
             }
         }
     }
