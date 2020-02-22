d6888fc8e1c985efecd421e8ea84ce3f32d18824
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2020-02-22 10:09:44 +0000

    Update NeuralNetwork/Test/NeuralNetwork.Test/CNN/CnnNetworkUsingBackpropagation.cs

--- NeuralNetwork/Test/NeuralNetwork.Test/CNN/CnnNetworkUsingBackpropagation.cs
+++ NeuralNetwork/Test/NeuralNetwork.Test/CNN/CnnNetworkUsingBackpropagation.cs
@@ -27,7 +27,7 @@ namespace NeuralNetwork.Test.CNN
             _testOutputHelper = testOutputHelper;
         }
 
-        [Fact]
+        [RunnableInDebugOnly]
         public void PredictCatVsDog()
         {
             var inputR = new Layer2D((200, 200), new Layer[0]);
