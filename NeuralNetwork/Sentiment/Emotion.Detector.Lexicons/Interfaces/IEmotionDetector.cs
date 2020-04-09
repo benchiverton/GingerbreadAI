4acdade99ed4c648998c8aa3ce6414cf379a904f
Ben Chiverton <benc@calastone.com>
2020-04-09 12:25:37 +0100

    Rename all projects with GingerbreadAI

--- NeuralNetwork/Sentiment/Emotion.Detector.Lexicons/Interfaces/IEmotionDetector.cs
+++
@@ -1,9 +0,0 @@
-﻿using Emotion.Detector.Lexicons.Data;
-
-namespace Emotion.Detector.Lexicons.Interfaces
-{
-    public interface IEmotionDetector
-    {
-        EmotionData Detect(string text);
-    }
-}
